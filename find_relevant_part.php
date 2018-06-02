4d9a36c7e88fc61c82c4185503f3749885498174
ChinmayRai <cs5160615@iitd.ac.in>
2018-06-02 11:47:56 +0530

    changes for printing the complete url

--- find_relevant_part.php
+++ find_relevant_part.php
@@ -5,20 +5,25 @@ include_once('../simple_html_dom.php');
 $html = file_get_html('https://timesofindia.indiatimes.com/2018/5/25/archivelist/year-2018,month-5,starttime-43245.cms');
 
 ob_start();
-	
+	echo '[';
 foreach($html->find('td[width=49%]') as $e)
 {
 	$e->innertext . '<br>';
 	foreach($e->find('a') as $f) 
 	{
-		echo $f->href . '<br>';		
+		echo ', ';	
+		echo '"';
+		echo 'https://timesofindia.indiatimes.com';
+		echo $f->href . '"';
+		echo "\n";
 	}
 }
+echo ']';
 $htmlStr = ob_get_contents();
 
 ob_end_clean(); 
 
-file_put_contents('result.html', $htmlStr);
+file_put_contents('result.json', $htmlStr);
 
 
 ?>
