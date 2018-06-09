978d7080954c82998e8c2806bc6f1bdfcc2d60ac
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-06-09 14:40:37 +0530

    removed redundant comments

--- web_crawling/heading_extractor/headings.py
+++ web_crawling/heading_extractor/headings.py
@@ -10,30 +10,8 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
             title = response.xpath('//td[@width="49%"]/span/a/text()').extract()
             url = response.xpath('//td[@width="49%"]/span/a/@href').extract()
-            # title = response.xpath('//td[@align="left"]/span/a/text()').extract()
-            # location = response.xpath('//div[@id="artLocation"]/div/text()').extract_first()
-            # source = response.xpath('//div[@id="artSource"]/div/text()').extract_first()
-            # top_line = response.xpath('//div[@id="divArticleContent"]/text()').extract_first()
-            # lines = response.xpath('//div[@id="divArticleContent"]/p/text()').extract()
         
             yield {
             'title' : title,
             'url' : url
             }
-
-            # yield {
-            # 'title' : title,
-            # 'location' : location,
-            # 'source' : source,
-            # 'top-line':  top_line,
-            # 'lines':  lines,
-        # }
-
-    # def parse(self, response):
-    #         yield {
-    #         'title' : response.xpath('//div[@id="divTitle"]/text()').extract_first(),
-    #         'location' : response.xpath('//div[@id="artLocation"]/div/text()').extract_first(),
-    #         'source' : response.xpath('//div[@id="artSource"]/div/text()').extract_first(),
-    #         'top-line':  response.xpath('//div[@id="divArticleContent"]/text()').extract_first(),
-    #         'lines':  response.xpath('//div[@id="divArticleContent"]/p/text()').extract(),
-    #     }
