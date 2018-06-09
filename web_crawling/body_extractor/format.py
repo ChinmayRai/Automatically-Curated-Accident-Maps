0a8138fe78275eeb63acf98b8f1d41c125bbe047
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-06-09 12:14:50 +0530

    updated format.py to remove newlines from body of articles

--- web_crawling/body_extractor/format.py
+++ web_crawling/body_extractor/format.py
@@ -1,14 +1,14 @@
 import json
 
-# def filter(s):
-# 	s2 = ""
-# 	for i in s:
-# 		if(s[i]=='\n')
-# 			i+=1
-# 			continue
-# 		else if(s[i]==' ' and i+1<len(s) and s[i+1])
-# 			if(i+1==len(s))
-
+def remove_newlines(s):
+	s2 = ""
+	for i in range(len(s)):
+		if(s[i]=='\n'):
+			i+=1
+		else:
+			s2 += s[i]
+			i+=1
+	return s2
 
 
 
@@ -21,12 +21,13 @@ l=[]
 for i in j:
 	l.append(i['lines'])
 
-q = l[1]
-# print q
+body = ['']*len(l)
 
+for i in range(len(l)):
+	s = ""
+	for line in l[i]:
+		s += line
+	body[i] = remove_newlines(s)
 
-s = ""
-for i in q:
-	s += i
-
-print s
+for i in body:
+	print i
