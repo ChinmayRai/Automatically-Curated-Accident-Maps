aa8362bd4c194bd39019625c852068e6d84611e3
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-06-09 16:41:18 +0530

    added comments in sample file to demonstrate use of scrapy spider as a function

--- web_crawling/heading_extractor/main.py
+++ web_crawling/heading_extractor/main.py
@@ -1,7 +1,11 @@
 import headlines
 
+# just add whatever comes after 'https://timesofindia.indiatimes.com/' in URL in a string
 query = "2018/5/26/archivelist/year-2018,month-5,starttime-43246.cms"
+# and pass that string as a parameter to headlines.extract to recieve an array
 arr = headlines.extract(query)
 
-for i in range(5):
-	print arr[i]
+
+# do whatever with that array now
+# for i in range(5):
+# 	print arr[i]
