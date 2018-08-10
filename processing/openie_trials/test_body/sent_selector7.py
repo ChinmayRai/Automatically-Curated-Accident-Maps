486825136b1a360397de691555983140225ceea0
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-08-10 14:11:11 +0530

    updated prep_break in data sets of july 2017

--- processing/openie_trials/test_body/sent_selector7.py
+++ processing/openie_trials/test_body/sent_selector7.py
@@ -101,10 +101,36 @@ def root_verbs(sentence):
 	return rootVerbs
 
 # breaks a string at prepositions and returns the list of clauses except the first clause.
+# def prep_break(s):
+# 	delimiters = ["into","in","on","at","near","under","around"]
+# 	# along(!along with)
+# 	s=s.replace(',','')
+# 	token="xdelimx"
+# 	for i in delimiters:
+# 		delim=" "+i+" "
+# 		s=s.replace(delim,token)
+# 		delim=i+" "
+# 		if(s[:len(delim)]==delim):
+# 			l=list(s)
+# 			l[:len(delim)]=list(token)
+# 			s=''.join(l)
+
+# 	l=s.split("xdelimx")
+# 	return l[1:]
 def prep_break(s):
-	delimiters = ["into","in","on","at","near","under","around"]
-	# along(!along with)
+	delimiters = ["into","in","on","at","near","under","around","along"]
+
+	# preprocessing the sentence
 	s=s.replace(',','')
+	s=s.lower()
+	# to remove full stop at the end of sentence
+	# if(s[-1]=='.'):
+	# 	s=s[:-1]
+
+	alongWithMarker="xcode1awx"
+	s=s.replace("along with", alongWithMarker);
+
+	# replacing prepositions by a delimiter
 	token="xdelimx"
 	for i in delimiters:
 		delim=" "+i+" "
@@ -115,9 +141,10 @@ def prep_break(s):
 			l[:len(delim)]=list(token)
 			s=''.join(l)
 
+	s=s.replace(alongWithMarker, "along with");
+
 	l=s.split("xdelimx")
 	return l[1:]
-
 
 # infile="july2017/july_openie_output.txt"
 # txtOutfile="july2017/july_sent_selected.txt"
