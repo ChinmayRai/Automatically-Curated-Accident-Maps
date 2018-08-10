09362023b5108a54e72e94c379f87fc44be54e84
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-08-10 15:56:43 +0530

    added casuality extraction crude version

--- processing/openie_trials/test_body/sent_selector7_casualty.py
+++ processing/openie_trials/test_body/sent_selector7_casualty.py
@@ -135,8 +135,8 @@ def prep_break(s):
 # jsonOutfile="july2017/july_sent_selected.json"
 
 infile="july2017/july_openie_output.txt"
-txtOutfile="july2017/july_sent_selected.txt"
-jsonOutfile="july2017/july_sent_selected.json"
+txtOutfile="july2017/july_sent_selected_casualty.txt"
+jsonOutfile="july2017/july_sent_selected_casualty.json"
 
 # whether or not to print the tuples of openie output
 printOpenieTuples = False
@@ -151,14 +151,14 @@ file_object.close()
 # set of verbs that indicate if sentence is to be selected
 # keyVerbs = ["kill","injure","die","hit","ram","travel","say","occur","suffer","overturn","crash","fell","collide","return","sustain","carry","lose","happen","run","head","turn","crush","succumb","smash"]
 
-keyVerbs1 = ["kill","injure","die","hit","ram","occur","dash","overturn","crash","fell","fall","collide","happen","run","plunge","crush","mow","skid","knock","smash","tumble","topple","turn","plough","plow","cram","hurt"]
-score1 = 3
+keyVerbs1 = ["kill","die","succumb"]
+# score1 = 3
 
-keyVerbs2 = ["suffer","sustain","lose","end"]
-score2 = 2
+keyVerbs2 = ["suffer","sustain","injure","hurt"]
+# score2 = 2
 
-keyVerbs3 = ["say","travel","head","succumb"]
-score3 = 1
+# keyVerbs3 = ["say","travel","head","succumb"]
+# score3 = 1
 
 maxSentFromArticle = 3
 sentence=[]
@@ -172,120 +172,54 @@ for line in lines:
 	if(line[0]=='-'):
 		if(len(sentence)>0):
 
-			flag=False
-			for t in sentence[1:]:
-				# t=get_tuple(i)
-				# if(t!=False):
-				if(t[1]=="took" and t[2]=="place" and len(t)>3):
-					flag=True
-				elif(t[1]=="met" and t[2][:4]=="with" and len(t)>3):
-					flag=True
-
-			# rootVerbs = extract(sentence)
-			rootVerbs=root_verbs(sentence[1:])
+			selectedTuples=[]
+			for tup in sentence[1:]:
+				rootVerbs=root_verbs([tup])
 			
-			if(flag):
-				# assigning max score to this sentence so that it is always taken unless there are more than maxSentFromArticle number of such sentences
-				score.append(sys.maxint)
-			else:
-				currScore = 0
-
 				inter1=intersection(rootVerbs,keyVerbs1)
 				inter2=intersection(rootVerbs,keyVerbs2)
-				inter3=intersection(rootVerbs,keyVerbs3)
-				
-				currScore += len(inter1)*score1
-				currScore += len(inter2)*score2
-				currScore += len(inter3)*score3
-				l=len(inter1)+len(inter2)+len(inter3)
-				if(l):
-					currScore /= l
-
-				score.append(currScore)
-
-			# sentence.append(rootVerbs)
-			for i in range(len(sentence[1:])):
-				index=1+i
-				d=dict()
-				d['tuple']=sentence[index]
-				for cl in sentence[index]:
-					if(cl[:2]=="L:"):
-						d['L']=cl[2:]
-					if(cl[:2]=="T:"):
-						d['T']=cl[2:]
-				sentence[index]=d
-
-			posSentence=[]
-			posSentence.append(prep_break(sentence[0]))
-			for tup in sentence[1:]:
-				s="".join(tup['tuple'][2:])
-				s=s.replace("T:"," ")
-				s=s.replace("L:"," ")
-				
-				l=prep_break(s)
-				if(l!=[]):
-					posSentence.append(l)
-
-				# try:
-				# 	s="".join(tup['tuple'][2:])
-				# 	posSentence.append(prep_break(s))
-				# except Exception as e:
-				# 	print e
-				# 	print tup['tuple']
-			# sentence.append(posSentence)
-			
-			# if printOpenieTuples==False then add only the original sentence to the article followed by list of clauses seperated by prepositions
-			if(printOpenieTuples):
-				article.append(sentence+posSentence)
-			else:
-				article.append([sentence[0]]+posSentence)
-
-			# to print intersection of root verbs with key verbs
-			# if(not(flag)):
-			# 	rv=set()
-			# 	for i in inter1:
-			# 		rv.add(i)
-			# 	for i in inter2:
-			# 		rv.add(i)
-			# 	for i in inter3:
-			# 		rv.add(i)
-			# 	article.append([list(rv)])
+				if(len(inter1)>0 or len(inter2)>0):
+					selectedTuples.append(tup[0])
+			if(len(selectedTuples)>0):
+				article.append(selectedTuples)
+				selectedTuples=[]
 			sentence=[]
 
 	elif(line[0]=='='):
-		usedIndices=[]
-		if(len(article)<=maxSentFromArticle):
-			selectedSentences=article
-		else:
-			tookPlaceFlag = False
-			for iter in range(maxSentFromArticle):
-				max = -sys.maxint - 1
-				for i in range(len(score)):
-					if(i in usedIndices):
-						continue
-					elif(max<score[i]):
-						max=score[i]
-						maxIndex=i
-				if(max==0):
-					break 	# not selecting sentences with score<=0
-				elif(max==sys.maxint and tookPlaceFlag==False):
-					tookPlaceFlag=True
-					usedIndices.append(maxIndex)
-					# print len(article), maxIndex
-					selectedSentences.append(article[maxIndex])
-				elif(max!=sys.maxint):
-					usedIndices.append(maxIndex)
-					# print len(article), maxIndex
-					selectedSentences.append(article[maxIndex])
-				else:
-					usedIndices.append(maxIndex)
-					# when max=sys.maxint and tookPlaceFlag=True then just add maxIndex to usedIndices
+		# usedIndices=[]
+		# if(len(article)<=maxSentFromArticle):
+		# 	selectedSentences=article
+		# else:
+		# 	tookPlaceFlag = False
+		# 	for iter in range(maxSentFromArticle):
+		# 		max = -sys.maxint - 1
+		# 		for i in range(len(score)):
+		# 			if(i in usedIndices):
+		# 				continue
+		# 			elif(max<score[i]):
+		# 				max=score[i]
+		# 				maxIndex=i
+		# 		if(max==0):
+		# 			break 	# not selecting sentences with score<=0
+		# 		elif(max==sys.maxint and tookPlaceFlag==False):
+		# 			tookPlaceFlag=True
+		# 			usedIndices.append(maxIndex)
+		# 			# print len(article), maxIndex
+		# 			selectedSentences.append(article[maxIndex])
+		# 		elif(max!=sys.maxint):
+		# 			usedIndices.append(maxIndex)
+		# 			# print len(article), maxIndex
+		# 			selectedSentences.append(article[maxIndex])
+		# 		else:
+		# 			usedIndices.append(maxIndex)
+		# 			# when max=sys.maxint and tookPlaceFlag=True then just add maxIndex to usedIndices
 
 
-		day.append(selectedSentences)
+		# day.append(selectedSentences)
+		day.append(article)
 		article=[]
-		selectedSentences=[]
-		score=[]
+		# selectedSentences=[]
+		# score=[]
 		# should not be reqd but adding it as sometimes openIE output is not as expected.
 		sentence=[]
 	elif(line[0]=='+'):
