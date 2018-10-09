ddfc54bb6950fda126b562c9650567d399d9d410
Samarth Aggarwal <samarthaggarwal2510@gmail.com>
2018-10-09 23:43:32 +0530

    added geocoding code using google geocoding api

---
+++ plotting/loc2geo.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+
+inFile="location.json"
+
+file_object = open(inFile,"r")
+locations = file_object.read()
+locations = json.loads(locations)
+file_object.close()
+
+coordinates=[]
+API_KEY="AIzaSyABji3II-_zS0WWLrDvCvU5zrfsw3IX9_0"
+
+for i in locations:
+	l=i['loc']
+	l=l.replace(' ','+')
+	
+	# l=locations[0]['loc']
+	# l=l.replace(' ','+')
+	# print l
+
+	url="https://maps.googleapis.com/maps/api/geocode/json?address="+l+"&key="+API_KEY
+	try:
+		r = requests.get(url)
+		coor=r.json()['results'][0]['geometry']['location']
+		coordinates.append(coor)
+	except Exception:
+		print r.status_code
+		print i
+
+jsonOutfile="coordinates.json"
+file_object = open(jsonOutfile,"w+")
+file_object.close()
+
+with open(jsonOutfile, 'w+') as outfile:
+    json.dump(coordinates, outfile)	
